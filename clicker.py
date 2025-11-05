9160537f1aafd21870574c2d46250aef777d51d9
ihorkoi <koleev.igor@gmail.com>
2025-11-05 14:00:39 +0200

    Add basic mouse click

---
+++ clicker.py
@@ -0,0 +1,42 @@
+import time
+import threading
+
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+delay = 0.5
+button = Button.left
+start_key = KeyCode(char='a')
+end_key = KeyCode(char='b')
+
+
+class AutoClicker(threading.Thread):
+    def __init__(self, delay, button):
+        super().__init__()
+        self.delay = delay
+        self.button = button
+        self.active = True
+        self.mouse = Controller()
+
+    def exit(self):
+        self.active = False
+
+    def run(self):
+        while self.active:
+            self.mouse.click(self.button)
+            time.sleep(self.delay)
+
+
+clicker = AutoClicker(delay=delay, button=button)
+clicker.start()
+
+
+def on_press(key):
+    print(key)
+    if key == end_key:
+        clicker.exit()
+        return False
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
