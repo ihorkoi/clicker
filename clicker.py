e3fc40e5c2849070f0385b12c54e149d92863fba
ihorkoi <koleev.igor@gmail.com>
2025-11-07 18:49:55 +0200

    extend MacroReader

--- clicker.py
+++ clicker.py
@@ -1,13 +1,16 @@
 import time
 import threading
 
-from pynput.mouse import Button, Controller
-from pynput.keyboard import Listener, KeyCode
+import pynput.mouse as p_mouse
+import pynput.keyboard as p_keyboard
+
+# from pynput.mouse import Button, Controller, Listener
+# from pynput.keyboard import Listener, KeyCode
 
 delay = 0.5
-button = Button.left
-start_key = KeyCode(char='a')
-end_key = KeyCode(char='b')
+button = p_mouse.Button.left
+start_key = p_keyboard.KeyCode(char='a')
+end_key = p_keyboard.KeyCode(char='b')
 
 
 class AutoClicker(threading.Thread):
@@ -15,8 +18,9 @@ class AutoClicker(threading.Thread):
         super().__init__()
         self.delay = delay
         self.button = button
+        # self.config = config
         self.active = True
-        self.mouse = Controller()
+        self.mouse = p_mouse.Controller()
 
     def exit(self):
         self.active = False
@@ -27,36 +31,54 @@ class AutoClicker(threading.Thread):
             time.sleep(self.delay)
 
 
-clicker = AutoClicker(delay=delay, button=button)
-clicker.start()
+# clicker = AutoClicker(delay=delay, button=button)
+# clicker.start()
 
 
 class MacroReader(threading.Thread):
     def __init__(self, toggle_key):
         super().__init__()
         self.active = True
-        self.record = {}
+        self.record_moves = []
+        self.record_clicks = []
+        self.record_scrolls = []
         self.toggle_key = toggle_key
-        pass
+        self.listener = p_mouse.Listener(on_move=self.on_move,
+                                         on_click=self.on_click,
+                                         on_scroll=self.on_scroll)
 
-    def run(self, key):
-        if key == self.toggle_key:
-            while self.active:
-                pass
+    def run(self):
+        self.listener.start()
+        self.listener.join()
 
-    def exit():
-        pass
+    def on_move(self, x, y):
+        if self.active:
+            self.record_moves.append({"x": x, "y": y})
+
+    def on_click(self, x, y, button, pressed):
+        print(self.record_clicks)
+        if self.active:
+            self.record_clicks.append({"x": x, "y": y, "button": button})
+
+    def on_scroll(self, x, y, dx, dy):
+        if self.active:
+            self.record_scrolls.append({"x": x, "y": y, "dx": dx, "dy": dy})
+
+    def exit(self):
+        self.active = False
 
     def save_config(self):
         pass
 
 
-def on_press(key):
-    print(key)
-    if key == end_key:
-        clicker.exit()
-        return False
+# def on_press(key):
+#     if key == end_key:
+#         clicker.exit()
+#         return False
 
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+# with p_keyboard.Listener(on_press=on_press) as listener:
+#     listener.join()
+
+recorder = MacroReader(p_keyboard.KeyCode(char='p'))
+recorder.start()
